b8a28459c2a8e1a7fd90637dc2324238ef9951b1
Matt Dobson <mdobson4@gmail.com>
2014-03-28 14:55:51 -0400

    Make initial example more positive.

--- ping.js
+++ ping.js
@@ -2,7 +2,7 @@ var serialPort = '/dev/tty.PebbleE8EA-SerialPortSe';
 var p = new Pebble(serialPort);
 p.on('open', function() {
   console.log('connection to pebble opened.');
-  p.email('libpebble', 'Hello, Pebble!', 'I hate you.', function(){
+  p.email('libpebble', 'Hello, Pebble!', 'I love you.', function(){
     console.log(arguments);
   });
 });
